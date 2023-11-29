cf459aecdfcb5625b10d4d5194bad6ebe5c76e20
Vitor <72328136+zVitorSantos@users.noreply.github.com>
2023-11-29 17:29:16 -0300

    Adicionado form de registro falta rotas ainda

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,58 +1,146 @@
 <template>
-    <div class="container mt-5 d-flex justify-content-center align-items-center">
-      <div class="login-form" style="max-width: 30%;">
-        <form>
-          <!-- Email input -->
-          <div class="form-outline mb-4">
-            <input type="email" id="email" class="form-control border-custom" />
-            <label class="form-label" for="email">Endereço de Email</label>
-          </div>
-  
-          <!-- Password input -->
-          <div class="form-outline mb-4">
-            <input type="password" id="password" class="form-control border-custom" />
-            <label class="form-label" for="password">Senha</label>
-          </div>
-  
-          <!-- 2 column grid layout for inline styling -->
-          <div class="row mb-4">
-            <div class="col d-flex justify-content-center">
-              <!-- Checkbox -->
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="remember" checked />
-                <label class="form-check-label" for="remember"> Me Lembre </label>
-              </div>
-            </div>
-  
-            <div class="col">
-              <!-- Simple link -->
-              <a href="#!">Esqueceu a senha?</a>
+  <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
+    <div class="login-form">
+      <form class="login-container" style="width: 19rem; max-width: 100%;" @submit.prevent="handleSubmit">
+
+        <div class="mb-4">
+          <input v-model="email" type="email" id="email" class="form-control border-custom" placeholder="Email" />
+        </div>
+
+        <div class="mb-4 position-relative">
+          <input v-model="password" type="password" id="password" class="form-control border-custom" placeholder="Senha" />
+          <i class="fas fa-eye-slash toggle-password" @click="togglePasswordVisibility"></i>
+        </div>
+
+        <div class="row mb-4">
+          <div class="col d-flex justify-content-center">
+            <div class="form-check">
+              <input v-model="rememberMe" class="form-check-input" type="checkbox" value="" id="remember" checked />
+              <label class="form-check-label" for="remember">Lembre-me</label>
             </div>
           </div>
-  
-          <!-- Submit button -->
-          <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-  
-          <!-- Register buttons -->
-          <div class="text-center">
-            <p>Não possúi conta? <a href="/register">Pedir Acesso</a></p>
+
+          <div class="col">
+            <!-- Simple link -->
+            <a href="#!">Esqueceu a senha?</a>
           </div>
-        </form>
-      </div>
+        </div>
+
+        <!-- Submit button -->
+        <button type="submit" class="btn btn-primary btn-block mb-4">Entrar</button>
+
+        <!-- Register buttons -->
+        <div class="text-center">
+          <p>Não possui conta? <a href="/register">Pedir Acesso</a></p>
+        </div>
+
+        <!-- Incorporando o componente de notificação -->
+        <Notification ref="notification" />
+      </form>
     </div>
-  </template>
-  
-  <style>
-  .login-form {
-    height: 100%;
-    width: 100%;
-    max-width: 400px;
-    margin: auto;
+  </div>
+</template>
+
+<script setup>
+import Notification from '../components/Notification.vue';
+
+// Definindo o estado do componente
+let email = '';
+let password = '';
+let rememberMe = true;
+
+// Função para exibir a notificação
+function showNotification(message) {
+  // Chama o método showNotification do componente de notificação
+  refs.notification.showNotification(message);
+}
+
+// Função para alternar a visibilidade da senha
+function togglePasswordVisibility() {
+  const passwordInput = document.getElementById('password');
+  const icon = document.querySelector('.toggle-password');
+
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    icon.classList.remove('fa-eye-slash');
+    icon.classList.add('fa-eye');
+  } else {
+    passwordInput.type = 'password';
+    icon.classList.remove('fa-eye');
+    icon.classList.add('fa-eye-slash');
   }
-  
-  /* Aumentando a especificidade para a classe border-custom */
-  .form-outline .border-custom {
-    border: 1px solid ;
-    border-radius: 5px;
+}
+
+// Função para lidar com o envio do formulário
+function handleSubmit() {
+  // Verifique se os campos estão preenchidos
+  if (!email || !password) {
+    showNotification('Por favor, preencha todos os campos.');
+    return;
   }
-  </style>
+
+  // Simule a lógica de login aqui (substitua pelo seu código real)
+  // Se houver um erro de login, exiba uma notificação
+  const loginSuccessful = true; // Substitua por sua lógica real
+  if (!loginSuccessful) {
+    showNotification('Erro ao fazer login. Verifique suas credenciais.');
+  }
+}
+</script>
+
+<style>
+.container-fluid {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-form {
+  height: 100%;
+  max-width: 400px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-container {
+  padding: 1rem;
+  border: 1px solid rgba(128, 128, 128, 0.8);
+  border-radius: 5px;
+  background-color: rgb(243, 243, 243);
+}
+
+.border-custom {
+  background-color: white;
+  border: 1px solid rgba(128, 128, 128, 0.8);
+  border-radius: 5px;
+  color: rgba(128, 128, 128, 0.8);
+}
+
+.border-custom:focus,
+.border-custom:valid {
+  border: 1px solid rgba(128, 128, 128, 0.8);
+}
+
+.border-custom::placeholder {
+  color: rgba(128, 128, 128, 0.8);
+}
+
+.row .col {
+  padding-left: 0;
+  padding-right: 0;
+}
+
+.toggle-password {
+  cursor: pointer;
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: white; 
+  padding: 5px;
+}
+</style>
--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,56 +1,121 @@
 <template>
-    <v-container fluid>
-      <v-layout row wrap>
-        <v-flex xs12 class="text-xs-center" mt-5>
-          <h1>Registrar-se</h1>
-        </v-flex>
-        <v-flex xs12 sm6 offset-sm3 mt-3>
-          <form>
-            <v-layout column>
-               <v-flex>
-                <v-text-field
-                  name="name"
-                  label="Nome"
-                  id="name"
-                  type="name"
-                  required></v-text-field>
-              </v-flex>
-              <v-flex>
-                <v-text-field
-                  name="email"
-                  label="Email"
-                  id="email"
-                  type="email"
-                  required></v-text-field>
-              </v-flex>
-              <v-flex>
-                <v-text-field
-                  name="password"
-                  label="Password"
-                  id="password"
-                  type="password"
-                  required></v-text-field>
-              </v-flex>
-              <v-flex>
-                <v-text-field
-                  name="confirmPassword"
-                  label="Confirm Password"
-                  id="confirmPassword"
-                  type="password"
-                  required
-                  ></v-text-field>
-              </v-flex>
-              <v-flex class="text-xs-center" mt-5>
-                <v-btn color="primary" type="submit">Sign Up</v-btn>
-              </v-flex>
-            </v-layout>
-          </form>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </template>
-  
-  <script>
-  export default {};
-  </script>
-  
+  <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
+    <div class="register-form">
+      <form class="register-container" style="width: 19rem; max-width: 100%;">
+
+        <div class="mb-4">
+          <input type="text" id="name" class="form-control border-custom" placeholder="Nome" />
+        </div>
+
+        <div class="mb-4">
+          <input type="email" id="email" class="form-control border-custom" placeholder="Email" />
+        </div>
+
+        <div class="mb-4 position-relative"> <!-- Adicionei a classe position-relative aqui -->
+          <input type="password" id="password" class="form-control border-custom" placeholder="Senha" />
+          <i class="fas fa-eye-slash toggle-password" onclick="togglePasswordVisibility('password', this)"></i>
+        </div>
+
+        <div class="mb-4 position-relative"> <!-- Adicionei a classe position-relative aqui -->
+          <input type="password" id="confirmPassword" class="form-control border-custom" placeholder="Repetir Senha" />
+          <i class="fas fa-eye-slash toggle-password" onclick="togglePasswordVisibility('confirmPassword', this)"></i>
+        </div>
+
+        <!-- User Type selection -->
+        <div class="form-outline mb-4">
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="userType" id="client" value="client" checked />
+            <label class="form-check-label" for="client">Cliente</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="userType" id="employee" value="employee" />
+            <label class="form-check-label" for="employee">Funcionário</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="userType" id="admin" value="admin" />
+            <label class="form-check-label" for="admin">Administrador</label>
+          </div>
+        </div>
+
+        <!-- Submit button -->
+        <button type="button" class="btn btn-primary btn-block mb-4">Registrar</button>
+
+        <!-- Register buttons -->
+        <div class="text-center">
+          <p>Já possui conta? <a href="/login">Entrar</a></p>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    function togglePasswordVisibility(inputId, icon) {
+      const passwordInput = document.getElementById(inputId);
+
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+      } else {
+        passwordInput.type = "password";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+      }
+    }
+
+    // Adiciona a função ao escopo global para que seja acessível no HTML
+    window.togglePasswordVisibility = togglePasswordVisibility;
+  });
+</script>
+
+<style>
+.container-fluid {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.register-form {
+  height: 100%;
+  max-width: 400px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.register-container {
+  padding: 1rem;
+  border: 1px solid rgba(128, 128, 128, 0.8);
+  border-radius: 5px;
+  background-color: rgb(243, 243, 243);
+}
+
+.border-custom {
+  background-color: white;
+  border: 1px solid rgba(128, 128, 128, 0.8);
+  border-radius: 5px;
+  color: rgba(128, 128, 128, 0.8);
+}
+
+.border-custom:focus,
+.border-custom:valid {
+  border: 1px solid rgba(128, 128, 128, 0.8);
+}
+
+.border-custom::placeholder {
+  color: rgba(128, 128, 128, 0.8);
+}
+
+.toggle-password {
+  cursor: pointer;
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+</style>
