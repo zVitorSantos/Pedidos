35e321109e85509a8f5ec704ef37b84f0a18c0d4
Vitor <72328136+zVitorSantos@users.noreply.github.com>
2023-12-12 16:38:23 -0300

    iniciando settings do usuário

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -1,16 +1,49 @@
 <template>
-    <div>
-        <!-- Your settings component code here -->
+    <div class="container-fluid">
+      <div class="row flex-nowrap">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+          <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+            <router-link to="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+              <span class="fs-5 d-none d-sm-inline">Menu</span>
+            </router-link>
+            <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+              <li class="nav-item">
+                <router-link to="/settings/profile" class="nav-link align-middle px-0">
+                <i class="fs-4 bi-house text-start"></i> <span class="ms-1 d-none d-sm-inline">Perfil</span>
+                </router-link>
+              </li>
+              <!-- Add more links here for other settings sections -->
+            </ul>
+          </div>
+        </div>
+        <div class="col py-3">
+          <!-- The settings content will be displayed here -->
+          <router-view></router-view>
+        </div>
+      </div>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     name: 'Settings',
     // Your component logic here
-}
-</script>
-
-<style scoped>
-/* Your component styles here */
-</style>
+  }
+  </script>
+  
+  <style scoped>
+  .settings {
+    display: flex;
+  }
+  
+  .sidebar {
+    width: 200px;
+    padding: 1rem;
+    border-right: 1px solid #ccc;
+  }
+  
+  .content {
+    flex-grow: 1;
+    padding: 1rem;
+  }
+  </style>
--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -2,37 +2,45 @@
     <div>
         <NavBarAuth />
         
-        <Config />
+        <Settings />
 
         <Footer />
     </div>
 </template>
 
-<script>
-import NavBarAuth from "../components/NavBarAuth.vue";
-import Config from "../components/Settings.vue";
-import Footer from "../components/Footer.vue";
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
 
-export default {
-    name: "Config",
-    components: {
-        NavBarAuth,
-        Footer,
-    },
-    beforeRouteEnter(to, from, next) {
-        // Check if the user is authorized
-        if (!isAuthorized()) {
-            next("/login");
-        } else {
-            next();
-        }
-    },
-};
+import NavBarAuth from '../components/NavBarAuth.vue';
+import Settings from '../components/Settings.vue';
+import Footer from '../components/Footer.vue';
 
-function isAuthorized() {
-    // Implement your authorization logic here
-    // Return true if the user is authorized, false otherwise
-}
+let isAuthenticated = ref(false);
+const router = useRouter();
+
+onMounted(async () => {
+  try {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      const response = await axios.get('http://127.0.0.1:5173/api/auth/verify-token', { 
+        headers: { Authorization: `Bearer ${token}` }
+      });
+
+      if (response.data && 'logged_in_as' in response.data) {
+        isAuthenticated.value = true;
+      } else {
+        router.push('/login');
+      }
+    } else {
+      router.push('/login');
+    }
+  } catch (error) {
+    console.error(error);
+    router.push('/login');
+  }
+});
 </script>
 
 <style>
